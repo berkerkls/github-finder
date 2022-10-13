b50faba874e3afb3444025480dce80d3adb3a1d9
Berker Kelesoglu <100119877+berkerkls@users.noreply.github.com>
2022-10-13 23:13:09 +0300

    pages added and notFound page completed

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h1 className='text-6xl mb-4 text-white'>Github Finder</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate exercitationem cum nesciunt laboriosam alias consequatur quibusdam corrupti ab. Dolor iure iusto, dicta repellendus totam consequuntur laboriosam rem, atque provident nulla, officia fugiat illum expedita perferendis eligendi eveniet necessitatibus nostrum? Dignissimos inventore aliquid ratione vitae eius possimus accusantium natus ut!</p>
+      <p>Version: <span className='text-white'>Latest</span> </p>
+    </div>
+  )
+}
+
+export default About
