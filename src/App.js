52942af0ac8940da27c488acc235d28d23c8646d
Berker Keleşoğlu <berkerkelesoglu@Berkers-MacBook-Air.local>
2023-02-05 21:56:02 +0300

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="default">
-      <div className="text-xl">Test</div>
-      <button className="btn btn-primary">Click</button>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>Content</main>
+      </div>
+    </Router>
   );
 }
 
