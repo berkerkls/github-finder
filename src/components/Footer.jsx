3def3c520dd06f6f95228086b21300eee107dbcb
Berker Keleşoğlu <berkerkelesoglu@Berkers-MacBook-Air.local>
2023-02-05 22:27:23 +0300

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  const footerYear = new Date().getFullYear();
+  return (
+    <footer className="footer p-10 bg-gray-700 text-primary-content footer-center">
+      <div>Github Finder || Copyright {footerYear}</div>
+    </footer>
+  );
+}
+
+export default Footer;
