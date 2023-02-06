19f5e92ef36deb6c3973a85e49f8ac19ba5ad940
Berker Keleşoğlu <berkerkelesoglu@Berkers-MacBook-Air.local>
2023-02-06 07:15:39 +0300

    trial

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,10 +13,10 @@ function Navbar({ title }) {
         </div>
         <div className="flex-1 px-2 mx-2">
           <div className="flex justify-end">
-            <Link to="/" className="btn-ghost btn-sm rounded-btn">
+            <Link to="/" className="btn-ghost btn-md rounded-btn">
               Home
             </Link>
-            <Link to="/about" className="btn-ghost btn-sm rounded-btn">
+            <Link to="/about" className="btn-ghost btn-md rounded-btn">
               About
             </Link>
           </div>
